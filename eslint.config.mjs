77e3be920a36ee578cba302e0a229cedd5212544
James Buchanan (Young) <140830722+jamesbuch@users.noreply.github.com>
2024-09-14 19:49:07 +1000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,10 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+    {
+        // config with just ignores is the replacement for `.eslintignore`
+        ignores: ['**/build/**', '**/dist/**', '**/examples/**'],
+    },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
 );
